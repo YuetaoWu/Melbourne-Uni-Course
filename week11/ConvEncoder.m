728ede8b271b0eb5a3b830bdd187fd69c736c286
Billoier <yuetao.wu@hotmail.com>
2018-06-06 17:57:22 +1000

    delete week11

--- week11/ConvEncoder.m
+++
@@ -1,7 +0,0 @@
-function [EncodedCodeSequence] = ConvEncoder(SourceSequence,GenPoly)
-	
-	Trellis = poly2trellis(GenPoly);
-	
-	EncodedCodeSequence = convenc(SourceSequence,Trellis);
-	
-end
--- week11/DecodedCodeSequence.m
+++
@@ -1,9 +0,0 @@
-function [DecodedCodeSequence]=ConvDecoder(ReceivedSequence,Type)
-
-	tbl = 5*3;
-	if(Type == 'hard')
-		DecodedCodeSequence = vitdec(ReceivedSequence,Trellis,tbl,'cont','hard')
-	end
-	else
-		DecodedCodeSequence = vitdec(ReceivedSequence,Trellis,tbl,'cont','unquant')
-	end
